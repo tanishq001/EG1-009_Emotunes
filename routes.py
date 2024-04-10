b9e5a38d5226efdfa57914fc3596bc66351163ef
tanishq001 <cptanishq001@gmail.com>
2024-04-10 10:41:43 +0530

    Modified routes.py

--- routes.py
+++ routes.py
@@ -16,53 +16,39 @@ user=APIRouter(prefix='/api',tags=['Register'])
 @user.post("/register")
 def signup_user(username: str ,email:EmailStr ,password:str):
     if username=='' or password=='' or email=='':
-        raise  HTTPException(
-            status_code=401, detail="Fill Out All Details"
-        )
+       return JSONResponse(status_code=400,content={'status': True, "message": "Fill Out All Details"})
     userRepository=UserRepository()
     db_user= userRepository.get_user_by_username(username)
     if db_user:
-         raise HTTPException(
-            status_code=401, detail="Username Already Exists"
-        )
+         return JSONResponse(status_code=401,content={'status': True, "message": "Username Already Exists"})
     
     signup=UserModel(email=email,username=username,password=get_pass_hash(password),is_active=False)
     success=userRepository.create_user(signup)
     token=create_access_token(signup)
     SendEmailVerify.sendVerify(token,email)
     if success:
-        return JSONResponse(content={'status': True, "message": "User Created Successfully."})
+        return JSONResponse(content={'status': True, "message": "User Created Successfully. Please Verify on provided mail."})
     
     else:
-        raise HTTPException(
-            status_code=401, detail="User Not Created!!"
-        )
+        return JSONResponse(status_code=405,content={'status': True, "message": "User Not Created!!"})
 
 @user.post("/signinuser")
 def signin_user(username : str ,password:str):
     if username=='' or password=='':
-        raise  HTTPException(
-            status_code=401, detail="Fill Out All Details"
-        )
+        return JSONResponse(status_code=400,content={'status': True, "message": "Fill Out All Details"})
     userRepository = UserRepository()
     db_user = userRepository.get_user_by_username(username)
 
     if not db_user:
-        raise  HTTPException(
-            status_code=401, detail="User Not Registered"
-        )
-    
+        return JSONResponse(status_code=404,content={'status': True, "message": "User Not Registered"})
     if db_user["is_active"]==False:
-         raise  HTTPException(
-            status_code=401, detail="User Not Verified"
-        )
+         return JSONResponse(status_code=401,content={'status': True, "message": "User Not Verified"})
+
     if verify_pass(password,db_user['password']):
         token=create_access_token(UserModel(**db_user))
-        return {COOKIE_NAME:token,"token_type":"project"}
+        return JSONResponse(content={'status': True, "message": "Login Successfull.", COOKIE_NAME:token,"token_type":"project"})
     else:
-         raise  HTTPException(
-            status_code=401, detail="Password Incorrect"
-        )
+        return JSONResponse(status_code=401,content={'status': True, "message": "Password Incorrect"})
 
 
 @user.get("/{token}")
@@ -73,10 +59,11 @@ def verify_user(token):
     db_user=userRepository.get_user_by_username(username)
     if not username:
         raise  HTTPException(
-            status_code=401, detail="Credentials not correct"
+            status_code=401, detail="Credentials not correct."
         )
     
     if db_user['is_active']==True:
-        return "Your Account has been already activated"
+        return "Your Account has been already activated."
  
     userRepository.update_is_active(username)
+    return "User Verified Successfully."
